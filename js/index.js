45cff0028ff28c503909e5c06e644bb19cf9f19f
oUwLe <sebastian.ljung18@hotmail.com>
2025-06-02 10:29:44 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,18 +1,29 @@
 
-console.log(document);
+console.log(document);                                                                  // Länkar js filen till html
 
-let todos = []; // En tom array
+let todos = [];                                                                         // En tom array
 
-const buttonElem = document.querySelector("#add-to-do-button");
+const buttonElem = document.querySelector("#add-to-do-button");                         // Letar upp add-to-do id elementet i html.
 console.log(buttonElem);
 
-buttonElem.addEventListener("click", () => {
-    console.log("Now you clicked on the button!");
+function displayTodos() {                                                               //
+    for(let i = 0; i < todos.length; i++) {                                              //
+        console.log("Todo nr: ", i);                                                     //
+        console.log("Todo: ", todos[i]);                                                 //
+    }
+}
 
+buttonElem.addEventListener("click", () => {                                            // Click function
+    const todo = prompt("What do you want to do?");                                     // Ger en prompt där användaren skriver ett värde på variabeln "todo".
+        todos.push(todo);                                                               // Pushar in variablen "todo" i "todos" array.
+            console.log(todos);                                                         // Konsol loggar "todos" array.
+            console.log("Now you clicked on the button!");
+            
+    displayTodos();
 });
 
-const todo = prompt("What do you want to do?");     //Ger en prompt där användaren skriver ett värde på variabeln "todo".
+            
 
-todos.push(todo);               // Pushar in variablen "todo" i "todos" array.
+               
 
-console.log(todos);             // Konsol loggar "todos" array.
+             
